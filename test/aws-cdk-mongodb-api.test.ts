cc1e5855555f79760a9e692cfeae0f8fa58e5903
Yonatan Lavie <yonatan.lavie89@gmail.com>
2023-08-22 12:43:56 +0300

    recreate the file structre using cdk init app --language=typescript

---
+++ test/aws-cdk-mongodb-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkMongodbApi from '../lib/aws-cdk-mongodb-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-mongodb-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkMongodbApi.AwsCdkMongodbApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
